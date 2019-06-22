4163b7f431c3ac18871e0d202a67c186b4602f71
Emily DeWitt <emilykdewitt@gmail.com>
2019-06-22 14:15:15 -0500

    everything in the tutorial so far

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,9 @@
-// let's go!
+//this imports React from the node modules folder
+import React from 'react';
+import { render } from 'react-dom';
+import Router from './components/Router';
+
+import './css/style.css';
+
+render(<Router />, document.getElementById('main'));
+
